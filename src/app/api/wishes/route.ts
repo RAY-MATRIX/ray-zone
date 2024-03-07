426defbea0acac569358dd1d82057f7d784cc506
rayqu <rayqu.work@gmail.com>
2024-03-07 23:54:09 +1100

    update route

--- src/app/api/wishes/route.ts
+++ src/app/api/wishes/route.ts
@@ -5,8 +5,10 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   const response = await axios({
     method: 'get',
     url: `${process.env.WISH_API_URL}/wishes`,
+    headers: {
+      'Cache-Control': 'no-cache',
+    },
   })
-
   const result = response.data
   return NextResponse.json({ data: result })
 }
